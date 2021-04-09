bfebe6cf73b13bf775b5eed5c9fb34ee53af097e
adharamonzon <adharamonzon@gmail.com>
2021-04-09 16:16:44 +0200

    add structure and styles of works section, add structure to followme section

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let works = [];
+
+const getData = () => {
+  return fetch('../js/data.json')
+ .then(response => response.json())
+ .then(data => {
+    works = data;
+    paintWorks();
+   })
+ }
+
+const list = document.querySelector('.works-list');
+
+const paintWorks = () => {
+  let listCode = ''; 
+  for (work of works) {
+    listCode += `<li id=${work.id} class="work-list-item">`;
+    listCode += `<h4 class="work-list-item-name uppercase">${work.name}</h4>`;
+    listCode += `<h6 class="work-list-item-subtitle">${work.subtitle}</h6>`;
+    listCode += '<a class="work-list-item-btn">Know more</a>';
+    listCode += `</li>`;
+  }  
+ list.innerHTML = listCode;
+}
+
+getData();
