a90a101c78b93d3410f6fd4b32713c4671eb0307
adharamonzon <adharamonzon@gmail.com>
2021-05-02 13:35:53 +0200

    change rute to json file

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 let works = [];
 
 const getData = () => {
-  return fetch('../js/data.json')
- .then(response => response.json())
- .then(data => {
-    works = data;
-    paintWorks();
-   })
- }
+  return fetch('./data/data.json')
+    .then((response) => response.json())
+    .then((data) => {
+      works = data;
+      paintWorks();
+    });
+};
 
 const list = document.querySelector('.works-list');
 
 const paintWorks = () => {
-  let listCode = ''; 
+  let listCode = '';
   for (work of works) {
     listCode += `<li id=${work.id} class="work-list-item">`;
     listCode += `<h4 class="work-list-item-name uppercase">${work.name}</h4>`;
     listCode += `<h6 class="work-list-item-subtitle">${work.subtitle}</h6>`;
     listCode += '<a class="work-list-item-btn">Know more</a>';
     listCode += `</li>`;
-  }  
- list.innerHTML = listCode;
-}
+  }
+  list.innerHTML = listCode;
+};
 
 getData();
