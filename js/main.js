6e002fb0733169527dfcb66fffa34b4a4ed520a2
adharamonzon <adharamonzon@gmail.com>
2021-05-11 12:16:45 +0200

    add modal window with initial structure

--- js/main.js
+++ js/main.js
@@ -11,16 +11,43 @@ const getData = () => {
 
 const list = document.querySelector('.works-list');
 
+const trigerModal = document.querySelectorAll('.js-workBtn');
+
 const paintWorks = () => {
   let listCode = '';
   for (work of works) {
     listCode += `<li id=${work.id} class="work-list-item">`;
     listCode += `<h4 class="work-list-item-name uppercase">${work.name}</h4>`;
     listCode += `<h6 class="work-list-item-subtitle">${work.subtitle}</h6>`;
-    listCode += '<a class="work-list-item-btn">Know more</a>';
+    listCode += `<button id=${work.id} class="work-list-item-btn js-workBtn" data-bs-toggle="modal" data-bs-target="#exampleModal">Know more</button>`;
     listCode += `</li>`;
   }
   list.innerHTML = listCode;
+  showModal();
 };
+const modalWindow = document.querySelector('.modal-content');
+const showModal = () => {
+  const modalTriger = document.querySelectorAll('.js-workBtn');
 
+  const handelModal = (ev) => {
+    debugger;
+    modalCard = '';
+    const btnTriger = parseInt(ev.currentTarget.id);
+    for (const work of works) {
+      console.log(works);
+      if (btnTriger === work.id) {
+        modalCard += `<div class="modal-header" id=${work.id}>`;
+        modalCard += `<h5 class="modal-title" id="exampleModalLabel">${work.name}</h5>`;
+        modalCard += `</div>`;
+        modalCard += `<div modal-body> `;
+        modalCard += `<p class="modal-description">${work.subtitle}</p>`;
+        modalCard += '</div>';
+        modalCard += '<div class="modal-footer">';
+        modalCard += `</div>`;
+      }
+    }
+    modalWindow.innerHTML = modalCard;
+  };
+  modalTriger.forEach((item) => item.addEventListener('click', handelModal));
+};
 getData();
